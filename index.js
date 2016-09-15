0739bff42992d21dfc3c687ca8a98910cb1c4322
Luis Cardenas <info@luchohero.com>
2016-09-15 16:33:55 -0500

    Git sismus2
    deploy

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
+var http = require('http');
 var express = require('express');
+var parseString = require('xml2js').parseString;
 var app = express();
+
 app.set('port', (process.env.PORT || 5000));
-app.get('/', function(request, response) {
-  response.send(':D ');
+
+app.get("/", function (req, res) {
+
+    var gsaReq = http.get("http://127.0.0.1/eventos.xml", function (response) {
+        var completeResponse = '';
+
+        response.on('data', function (chunk) {
+            completeResponse += chunk;
+            //res.send(response.toString());
+            //res.setHeader('Content-Type', 'application/json');
+            //res.send(JSON.stringify(response));
+        });
+        response.on('end', function() {
+            //console.log(completeResponse);
+            parseString(completeResponse, function (err, result) {
+                console.dir(result);
+                var obj = result.markers.marker;
+                for(i in obj){
+                  console.log(obj[i].$);
+                }
+
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(result.markers.marker));
+            });
+        })
+    }).on('error', function (e) {
+        console.log('problem with request: ' + e.message);
+    });
+
 });
+
+//app.listen(3000);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
