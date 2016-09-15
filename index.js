cc28ed6cd540eb77e1f5ec8e9b9e433d4e4ac486
Luis Cardenas <info@luchohero.com>
2016-09-15 16:18:32 -0500

    s
    s

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-//var http = require('http');
 var express = require('express');
-
 var app = express();
-
 app.set('port', (process.env.PORT || 5000));
-
-app.get("/", function (req, res) {
-res.send("LUCHO HERO");
+app.get('/', function(request, response) {
+  response.send(':D');
 });
-
-//app.listen(3000);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
