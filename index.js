3dde1d46b8be9dfcd997fe07ed8d861163b47308
Luis Cardenas <info@luchohero.com>
2016-09-15 15:38:02 -0500

    prueba express2
    deploy

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
+//app.use(express.static(__dirname + '/public'));
+
+//app.use('/static', express.static('public'));
+app.get('/', function(request, response) {
   res.send('Hello World!');
 });
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
