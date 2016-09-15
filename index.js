8f8576cb3eae6901f435a975e5429d6ae8473c79
Luis Cardenas <info@luchohero.com>
2016-09-15 15:34:10 -0500

    prueba
    solo express

--- index.js
+++ index.js
@@ -1,42 +1,10 @@
-var http = require('http');
 var express = require('express');
-var parseString = require('xml2js').parseString;
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.get("/", function (req, res) {
-
-    var gsaReq = http.get("http://127.0.0.1/eventos.xml", function (response) {
-        var completeResponse = '';
-
-        response.on('data', function (chunk) {
-            completeResponse += chunk;
-            //res.send(response.toString());
-            //res.setHeader('Content-Type', 'application/json');
-            //res.send(JSON.stringify(response));
-        });
-        response.on('end', function() {
-            //console.log(completeResponse);
-            parseString(completeResponse, function (err, result) {
-                console.dir(result);
-                var obj = result.markers.marker;
-                for(i in obj){
-                  console.log(obj[i].$);
-                }
-
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(result.markers.marker));
-            });
-        })
-    }).on('error', function (e) {
-        console.log('problem with request: ' + e.message);
-    });
-
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-//app.listen(3000);
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
 });
