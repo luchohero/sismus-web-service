4764e2f98aae1a8e9f2f903a4e123c07891d9d71
Luis Cardenas <info@luchohero.com>
2016-09-15 23:58:09 -0500

    :D
    ..

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
 var http = require('http');
-var express = require('express');
-var parseString = require('xml2js').parseString;
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
 app.get("/", function (req, res) {
 
-    var gsaReq = http.get("http://127.0.0.1/eventos.xml", function (response) {
-        var completeResponse = '';
-
-        response.on('data', function (chunk) {
-            completeResponse += chunk;
-            //res.send(response.toString());
-            //res.setHeader('Content-Type', 'application/json');
-            //res.send(JSON.stringify(response));
-        });
-        response.on('end', function() {
-            //console.log(completeResponse);
-            parseString(completeResponse, function (err, result) {
-                console.dir(result);
-                var obj = result.markers.marker;
-                for(i in obj){
-                  console.log(obj[i].$);
-                }
-
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(result.markers.marker));
-            });
-        })
-    }).on('error', function (e) {
-        console.log('problem with request: ' + e.message);
-    });
-
+res.send("luis");
 });
 
 //app.listen(3000);
