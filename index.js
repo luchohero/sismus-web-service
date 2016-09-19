6e8eb0076b546542083f3f253c2b8c8f651e1b22
Luis Cardenas <info@luchohero.com>
2016-09-19 00:47:41 -0500

    Fecha Corregida
    :D

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ var http = require('http');
 var express = require('express');
 var parseString = require('xml2js').parseString;
 var app = express();
+function dateFormat(d)
+	{
+		return d.getDate()+"/"+d.getMonth()+"/"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+	}
+
+  function llenar_fecha(){
+    var f = new Date();
+    var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+    var diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+    return diasSemana[f.getDay()]+" "+f.getDate()+" de "+meses[f.getMonth()];
+  }
+
+
+
+  function llenar_hora(fecha){
+
+  if(fecha.getMinutes()<10){
+  var minutos = "0"+fecha.getMinutes();
+  }else{var minutos = fecha.getMinutes();}
+
+  if(fecha.getSeconds()<10){
+  var segundos= "0"+fecha.getSeconds();
+  }else{var segundos = fecha.getSeconds();}
+
+  return fecha.getHours()+":"+minutos+":"+segundos;
+  }
 
 app.set('port', (process.env.PORT || 8000));
 
@@ -24,11 +50,15 @@ app.get("/", function (req, res) {
                 var obj = result.markers.marker;
                 for(i in obj){
                   var fecha = new Date(obj[i].$.fecha);
-                  var imge = "http://www.igepn.edu.ec/portal/ultimo-sismo/event/"+obj[i].$.eventoid+"/"+obj[i].$.eventoid+"-gmapa.png";
-                  objeto.push({"id":obj[i].$.eventoid,"lat":obj[i].$.lat,"long":obj[i].$.lng,"mag":obj[i].$.mg,"prof":obj[i].$.z,"fecha":fecha,"hora":obj[i].$.fecha,"img":imge,"direccion":obj[i].$.localizacion});
-
+                  var tz=5;
+                  var seconds=(tz*60*60)*1000;
                   console.log(obj[i].$.fecha);
-                  console.log(fecha);
+                  fecha.setTime(fecha.getTime()-seconds);
+                  console.log(dateFormat(fecha));
+                  var imge = "http://www.igepn.edu.ec/portal/ultimo-sismo/event/"+obj[i].$.eventoid+"/"+obj[i].$.eventoid+"-gmapa.png";
+                  objeto.push({"id":obj[i].$.eventoid,"lat":obj[i].$.lat,"long":obj[i].$.lng,"mag":obj[i].$.mg,"prof":obj[i].$.z,"fecha":llenar_fecha(fecha),"hora":llenar_hora(fecha),"img":imge,"direccion":obj[i].$.localizacion});
+
+
                   //console.log(obj[i].$.lat);
                 }
                 //console.log(objeto);
